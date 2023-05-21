1cbc0b172932d5b0dcb5afdf77e280fe2e844a0f
oguilhermesilva2 <guilherme.asilva@sptech.school>
2023-05-21 17:26:44 -0300

    Tela de login do funcionario conectada na api

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "digitalTherm",
     user: "root",
-    password: "2153",
+    password: "Jcvive@1",
 };
 
 function executar(instrucao) {
